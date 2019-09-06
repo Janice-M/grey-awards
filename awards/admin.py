8bade7302bc991a77cdbd1c5086e4bd524565703
Janice-M <janiceink001@gmail.com>
2019-09-06 15:55:53 +0300

    working on admin an

--- awards/admin.py
+++ awards/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Project, Tags
 # Register your models here.
+
+Class ProjectAdmin(admin.ModelAdmin):
+    filter_horizontal=('tags')
+    
+    
+admin.site.register(Project,ProjectAdmin)
+admin.site.register(Tags)
