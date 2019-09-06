d45b811c5fd093340a98a298ef1f0b44a72323a5
Janice-M <janiceink001@gmail.com>
2019-09-06 16:01:10 +0300

    added tags model

--- awards/models.py
+++ awards/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import datetime as dt
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 
 # Create your models here.
+#####tags for projects on grey awards
+
+class Tags(models.Model):
+    name=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
