d0a22a496ae194874149802add03f3b6194cb29f
Janice-M <janiceink001@gmail.com>
2019-09-10 11:53:34 +0300

    worked on models

--- awards/admin.py
+++ awards/admin.py
@@ -1,16 +1,11 @@
 from django.contrib import admin
-from .models import categories,technologies,colors,countries,Project,Profile,Rating
+from .models import Project,Profile,Rating
 
 
-class ProjectAdmin(admin.ModelAdmin):
-    filter_horizontal =('technologies','categories','colors')
+    # filter_horizontal =('technologies','categories','colors')
 
 
 # Register your models here.
-admin.site.register(categories)
-admin.site.register(technologies)
-admin.site.register(colors)
-admin.site.register(countries)
-admin.site.register(Project,ProjectAdmin)
+admin.site.register(Project)
 admin.site.register(Profile)
 admin.site.register(Rating)
--- awards/views.py
+++ awards/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404,HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ObjectDoesNotExist
-from .models import categories,technologies,colors,countries,Project,Profile,Rating
+from .models import Project,Profile,Rating
 from .forms import ProjectForm,ProfileForm,RatingForm
 from decouple import config,Csv
 import datetime as dt
@@ -14,7 +14,7 @@ from django.contrib.auth.models import User
 
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializer import ProfileSerializer,ProjectSerializer,technologiesSerializer,colorsSerializer,countriesSerializer,categoriesSerializer
+from .serializer import ProfileSerializer,ProjectSerializer
 
 
 # Create your views here.
